ff48f6161b5da845e265ef52043a182c4bf56511
Murfew <matthew.hurley953@gmail.com>
2025-07-11 21:52:20 -0400

    fix file setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
