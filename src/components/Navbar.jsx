9b298e61b98652a91cd92c0b87ec58865b7d99f4
Murfew <matthew.hurley953@gmail.com>
2025-07-18 18:34:43 -0400

    added basic styles to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import { ShoppingCart } from "lucide-react"
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
   return (
-    <nav>
-      <h1>Creative Shop Name</h1>
-      <div id="pages">
-        <h2>Home</h2>
-        <h2>Shop</h2>
+    <header className="bg-background text-primary shadow">
+      <div className="max-w-6xl mx-auto px-6 flex justify-between items-center">
+        <h1 className="text-xl font-bold">FakeStore</h1>
+        <nav className="space-x-6">
+          <Link to={"/"} className="text-secondary hover:text-accent transition-colors">Home</Link>
+          <Link to={"/shop"} className="text-secondary hover:text-accent transition-colors">Shop</Link>
+        </nav>
+        <Link to={"/cart"}><ShoppingCart /></Link>
       </div>
-      <ShoppingCart />
-    </nav>
+    </header>
+
   )
 }
